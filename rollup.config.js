dd1df4274231246e380b150dfceb22b0b03d62b0
anaestheticsapp <49522559+anaestheticsapp@users.noreply.github.com>
2021-04-24 13:55:41 +0200

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import rimraf from 'rimraf';
+
+const isProduction = process.env.BUILD === 'production';
+
+function build(name = 'main') {
+  return {
+    input: 'src/idb.js',
+    output: {
+      dir: 'dist/',
+      format: 'esm',
+      plugins: [],
+      sourcemap: isProduction ? false : true,
+    },
+  };
+}
+
+console.log('process.env.BUILD', process.env.BUILD);
+rimraf.sync('dist/');
+
+export default build();
